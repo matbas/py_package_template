33dfc87b3997c589fabe1035a208c6426e70c53d
Matthew Bass <matbas80@gmail.com>
2017-07-27 12:13:46 +0100

    python project template

---
+++ {{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='{{ cookiecutter.project_name }}',
+    version='{{ cookiecuter.version }}',
+    description='{{ cookiecutter.description }}',
+    author='{{ cookiecutter.full_name }}',
+    author_email='{{ cookiecutter.email }}',
+    url='https://github.com/matbas/{{ cookiecutter.project_name }}',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
---
+++ {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_name}}.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+from {{ cookiecutter.project_name }} import {{ cookiecutter.project_name }}
+
+
+class Test{{ cookiecutter.project_name }}(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
